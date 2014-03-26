89d98c3642d1b78d9ff0e60b61988eb0f0dc8c10
Peter Smit <peter@smitmail.eu>
2014-03-26 14:48:52 +0200

    Initial script for processing duplicate file

---
+++ read_duplicate_file.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import fileinput
+import subprocess
+import time
+
+def process_duplicates(filenames):
+    if len(filenames) == 0:
+        return
+
+    command = ["bedup", "dedup-files"]
+    command.extend(filenames)
+    subprocess.call(command)
+
+def main():
+    cur_sha = ""
+    cur_files = []
+    for line in fileinput.input():
+        sha, filename = line.split(None, 1)
+        if sha == cur_sha:
+            cur_files.append(filename)
+        else:
+            process_duplicates(cur_files)
+            time.sleep(10)
+            cur_sha = sha
+            cur_files = []
+
+if __name__ == "__main__":
+    main()
